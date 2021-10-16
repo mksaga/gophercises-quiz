79b93687e5681d45bfc752d1e796cea493e9d88b
Mohamed Aly <mohamed.aly06@gmail.com>
2021-10-16 14:36:57 -0400

    Adds scanner to read lines from csv input

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"os"
 )
 
 func check(e error) {
@@ -12,13 +14,31 @@ func check(e error) {
 }
 
 func main() {
-	csvPtr := flag.String("csv", "problem.csv", "a csv file in format of 'question,answer'")
+	// Declare -csv and -limit flags
+	csvPtr := flag.String("csv", "problems.csv", "a csv file in format of 'question,answer'")
 	limitPtr := flag.Int("limit", 30, "time limit for quiz in seconds")
 
+	// Parse command line input into flags
 	flag.Parse()
 
 	fmt.Println("csv file: ", *csvPtr)
 	fmt.Println("limit: ", *limitPtr)
+
+	// Access the problem csv file
+	_, err := os.ReadFile(*csvPtr)
+	check(err)
+
+	// Actually open problem csv file
+	f, err := os.Open(*csvPtr)
+	check(err)
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err!= nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
 
 
 }
