e2fdbd5868623d173871a36b3e9cc208ad3c2442
Mohamed Aly <mohamed.aly06@gmail.com>
2021-10-16 14:30:21 -0400

    Adds check function

--- main.go
+++ main.go
@@ -5,13 +5,20 @@ import (
 	"fmt"
 )
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func main() {
 	csvPtr := flag.String("csv", "problem.csv", "a csv file in format of 'question,answer'")
 	limitPtr := flag.Int("limit", 30, "time limit for quiz in seconds")
 
 	flag.Parse()
-	// commandLineArgs := flag.Args()
 
 	fmt.Println("csv file: ", *csvPtr)
 	fmt.Println("limit: ", *limitPtr)
+
+
 }
